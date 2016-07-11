36019f9654d28238a51c9aa64d70349029e83c27
sirmdub <sirmdub@gmail.com>
2016-07-11 16:10:36 -0400

    usage notes for pickle

--- sampleCode/pickle.py
+++ sampleCode/pickle.py
@@ -5,6 +5,7 @@ import pickle
 
 pkl_file = open('data.pkl', 'rb')
 config = pickle.load(pkl_file)
+#subsequent calls will load multiple objects in sequence
 pkl_file.close()
 
 print(config.get("access","access_token"))
@@ -18,4 +19,5 @@ print(json.loads(config.get("app","friends")))
 
 output = open('data.pkl', 'wb')
 pickle.dump(config, output)
+#subsequent calls will dump multiple objects in sequence
 output.close()
