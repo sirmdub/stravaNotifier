4c8da0a6da8aadec200cfe3bea4956128c57d382
sirmdub <sirmdub@gmail.com>
2016-07-12 10:18:52 -0400

    json serialization code samplesj

--- sampleCode/persistence.py
+++ sampleCode/persistence.py
@@ -1,8 +1,10 @@
 import ConfigParser
 import json
+import io
 import pickle
 
 
+###pickle serialization examples###
 pkl_file = open('data.pkl', 'rb')
 config = pickle.load(pkl_file)
 #subsequent calls will load multiple objects in sequence
@@ -21,3 +23,17 @@ output = open('data.pkl', 'wb')
 pickle.dump(config, output)
 #subsequent calls will dump multiple objects in sequence
 output.close()
+
+
+
+###json serialization examples###
+made_json = json.dumps(["1234", "1766377", "7587139", "6853684", "11361212", "1755537"])
+of = open('basic.json', 'wb')
+json.dump(made_json, of)
+of.close()
+
+f = open('basic.json', 'rb')
+got_json = json.load(f)
+f.close()
+
+print(got_json)
